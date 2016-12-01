4829a7a26f4f17040b9a18e8118abb023684b65d
radlee <lolito@projectcodex.co>
2016-12-01 08:53:57 +0200

    Ajax

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var five = require("johnny-five");
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var app = express();
-var board = new five.Board();
+// var board = new five.Board();
 
 //setup template handlebars as the template engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -19,34 +19,34 @@ app.get("/",function(req, res) {
 });
 
 
-board.on("ready", function() {
-
-  var pullRequestLED = new five.Led(13);
-  var mergeLED = new five.Led(12);
-  pullRequestLED.on();
-
-  app.get("/pullRequestOn", function(req, res){
-    pullRequestLED.on();
-    res.render("home");
-  });
-
-  app.get("/mergeOn", function(req, res){
-    mergeLED.on();
-    res.render("home");
-  });
-
-  app.get("/pullRequestOff", function(req, res){
-    pullRequestLED.off();
-    res.render("home");
-  });
-
-  app.get("/mergeOff", function(req, res){
-    mergeLED.off();
-    res.render("home");
-  });
-
-
-});
+// board.on("ready", function() {
+//
+//   var pullRequestLED = new five.Led(13);
+//   var mergeLED = new five.Led(12);
+//   pullRequestLED.off();
+//
+//   app.get("/pullRequestOn", function(req, res){
+//     pullRequestLED.on();
+//     res.render("home");
+//   });
+//
+//   app.get("/mergeOn", function(req, res){
+//     mergeLED.on();
+//     res.render("home");
+//   });
+//
+//   app.get("/pullRequestOff", function(req, res){
+//     pullRequestLED.off();
+//     res.render("home");
+//   });
+//
+//   app.get("/mergeOff", function(req, res){
+//     mergeLED.off();
+//     res.render("home");
+//   });
+//
+//
+// });
 
 var port = process.env.port || 3000;
 app.listen(port, function(){
